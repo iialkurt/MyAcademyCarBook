60e12a167366e6a7fa31047b33ae5f0d3f5dc2a6
iialkurt <iialkurt@gmail.com>
2023-10-05 22:08:04 +0300

    Proje dosyası ekle.

---
+++ MyAcademyCarBook.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class     
+    {
+        void insert(T Entity);
+        void Update(T Entity);
+        void Delete(T Entity);
+        List<T> GetAll();
+        T GetByID(int id);
+    }
+    
+}
---
+++ MyAcademyCarBook.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using MyAcademyCarBook.DataAccessLayer.Abstract;
+using MyAcademyCarBook.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+       
+    {
+       CarBookContext context = new CarBookContext();
+   
+       
+        public void Delete(T Entity)
+        {
+           context.Set<T>().Remove(Entity);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+            
+
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void insert(T Entity)
+        {
+            context.Set<T>().Add(Entity);    
+            context.SaveChanges();  
+        }
+
+        public void Update(T Entity)
+        {
+            context.Set<T>().Update(Entity);
+            context.SaveChanges();
+        }
+    }
+}
